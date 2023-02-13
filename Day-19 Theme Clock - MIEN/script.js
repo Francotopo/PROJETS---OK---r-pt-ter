95790f32debd660abcf9cf868058e78fee99f221
Francotopo <pintofrancois@yahoo.fr>
2023-02-13 21:40:04 +0100

    [200~

---
+++ Day-19 Theme Clock - MIEN/script.js
@@ -0,0 +1,12 @@
+// ========= Variables ===================
+
+// ========= Arrays - Days-Months ============
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// ========= dark-light mode ============
+
+// ************************************************
+function scale(num, in_min, in_max, out_min, out_max) {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
