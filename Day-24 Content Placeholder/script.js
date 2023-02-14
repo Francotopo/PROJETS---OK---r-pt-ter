2e5b2983f622018544cf65d4eab593670461e445
Francotopo <pintofrancois@yahoo.fr>
2023-02-14 18:43:45 +0100

    ajour des dossiers Day-22 Drawing App + Day-23 Kinetic CSS Loader + Day-24 Content Placeholder

---
+++ Day-24 Content Placeholder/script.js
@@ -0,0 +1,3 @@
+// "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
+
+// "https://randomuser.me/api/portraits/men/45.jpg"
