2235bb4ea7b6d55922e957b4b03332b0975a51aa
Francotopo <pintofrancois@yahoo.fr>
2023-02-21 18:31:40 +0100

    final Day-44

--- Day-44 Custom Range Slider/script.js
+++ Day-44 Custom Range Slider/script.js
@@ -2,3 +2,28 @@
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+const range = document.getElementById('range');
+
+range.addEventListener('input', (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const rangeWidth = getComputedStyle(e.target).getPropertyValue('width');
+  const labelWidth = getComputedStyle(label).getPropertyValue('width');
+
+  const numRangeWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+  console.log(numRangeWidth);
+  const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
+
+  const maximum = +e.target.max;
+  const minimum = +e.target.min;
+
+  const left =
+    value * (numRangeWidth / maximum) -
+    numLabelWidth / 2 +
+    scale(value, minimum, maximum, 10, -10);
+  label.style.left = `${left}px`;
+
+  label.innerText = value;
+});
